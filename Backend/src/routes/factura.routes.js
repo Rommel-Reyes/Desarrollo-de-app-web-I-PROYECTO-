95e546d87852dd0700d3e2ec6e82b9312e2a69ff
Rommel Reyes <rommelalex1013@icloud.com>
2025-09-22 15:05:29 -0600

    Delete Backend directory

--- Backend/src/routes/factura.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as facturaController from "../controllers/factura.controller.js";
-import auth from "../middlewares/auth.middleware.js";
-
-const router = Router();
-
-router.get("/:idVenta", auth, facturaController.getFacturaByVenta);
-
-export default router;
